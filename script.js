2111a8ce8d36f9a3ad1c5006f4b27cb9fe797ecf
Sophiane2009 <sgaf2608@gmail.com>
2025-05-14 21:36:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const batiments = document.querySelectorAll(".batiment");
+  const fiche = document.getElementById("fiche");
+
+  batiments.forEach(function (batiment) {
+    batiment.addEventListener("click", function () {
+      const texte = batiment.getAttribute("data-fiche");
+      fiche.innerHTML = `<div class="fiche">${texte}</div>`;
+    });
+  });
+});
